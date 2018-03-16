10349054098939980dee69e12186c775d014c5c9
Andrew Paulino <andrewpaulino46@gmail.com>
2018-03-15 19:24:44 -0700

    Files and all dependencies initalized

---
+++ src/js/containers/ExContainer/ExContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="examplecontainer">
+                <h1> Testing Boiler Plate </h1> 
+            </div>
+        )
+    }
+}
